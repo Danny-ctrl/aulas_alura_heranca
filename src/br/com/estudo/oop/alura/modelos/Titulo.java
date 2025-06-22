f152544d3bd7b463b3957c3cde6345d23783e33b
Daniele Inspiron <contatodanny@hotmail.com>
2025-06-22 17:00:06 -0300

    classe serie e estendendo a classe titulo

--- src/br/com/estudo/oop/alura/modelos/Titulo.java
+++ src/br/com/estudo/oop/alura/modelos/Titulo.java
@@ -47,7 +47,7 @@ public class Titulo {
         }
 
         public void exibeFichaTecnica() {
-            System.out.println("Nome do filme: " + nome);
+            System.out.println("Nome: " + nome);
             System.out.println("Ano de lançamento: " + anoDeLancamento);
             System.out.println("Duração em minutos: " + duracaoEmMinutos);
             System.out.println("Incluído no plano: " + incluidoNoPlano);
